b24424b51dd4efaaa89ac923c3647b7a56cde50d
Abdisemed Abdullahi <abdisemed20@gmail.com>
2023-10-15 20:59:25 +0300

    completed assignmnent

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+const drivers=['Antonia', 'Nuru', 'Amari', 'Mo']
+const returnFirstTwoDrivers=function(drivers){
+  let newDrivers=drivers.slice(0,2);
+    return newDrivers;
+}
+const returnLastTwoDrivers=function(drivers){
+    let newDrivers=drivers.slice(2);
+      return newDrivers;
+  }
+  let  selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers]
+
+  function createFareMultiplier(integer){
+    return function fareMultiplier(fare){
+     let totalfare=fare*integer;
+     return totalfare;
+  }
+    
+  
+}
+console.log(createFareMultiplier()());
+  function fareDoubler(number){
+    let double=number*2; 
+    return double;
+  }
+  console.log(fareDoubler())
+  function fareTripler(number){
+    let triple=number*3; 
+    return triple;
+  }
+  console.log(fareTripler());
+  
+  function selectDifferentDrivers(drivers, fn) {
+    return fn(drivers);
+}
+  
+  
